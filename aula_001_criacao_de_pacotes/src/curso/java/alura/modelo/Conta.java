6efe7571d2541fa67ba4255419edd5cfd32a2824
wagner <wagner.santos@accurate.com.br>
2023-03-28 16:28:35 -0300

    Sobrescrevendo o metodo toString()

--- aula_001_criacao_de_pacotes/src/curso/java/alura/modelo/Conta.java
+++ aula_001_criacao_de_pacotes/src/curso/java/alura/modelo/Conta.java
@@ -85,4 +85,9 @@ public abstract class Conta {
     public static int getTotal() {
         return Conta.total;
     }
+
+    @Override
+    public String toString(){
+        return "Agencia " + this.getAgencia() + ", Conta "+ this.getNumero();
+    }
 }
---
+++ aula_001_criacao_de_pacotes/src/curso/java/alura/testes/Testes.java
@@ -0,0 +1,13 @@
+package curso.java.alura.testes;
+
+
+import curso.java.alura.modelo.ContaCorrente;
+
+public class Testes {
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente(23, 27);
+
+        System.out.println(cc);
+    }
+}
